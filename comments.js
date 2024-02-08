dec19f2fe002ef5a8ffd94cf4861d39145653c9e
Hemant Raj Singh <rajhemant84@gmail.com>
2024-02-08 06:04:13 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,54 +1,30 @@
-// Create web server
+// create web server for comment
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// Load the http module to create an http server.
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var path = require('path');
-var mime = require('mime');
-var qs = require('querystring');
-var comments = [];
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-    var pathname = url.parse(request.url).pathname;
-    if (pathname == '/') {
-        pathname = '/index.html';
-    }
-    if (pathname == '/index.html') {
-        fs.readFile(path.join(__dirname, pathname), function (err, data) {
-            if (err) {
-                response.writeHead(404, { 'Content-Type': 'text/html' });
-                response.end('<h1>404 Not Found</h1>');
-            }
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(data);
-        });
-    } else if (pathname == '/comments') {
-        if (request.method == 'GET') {
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify(comments));
-        } else if (request.method == 'POST') {
-            var str = '';
-            request.on('data', function (data) {
-                str += data;
-            });
-            request.on('end', function () {
-                var comment = qs.parse(str);
-                comments.push(comment);
-                response.end();
-            });
-        }
-    } else {
-        fs.readFile(path.join(__dirname, pathname), function (err, data) {
-            if (err) {
-                response.writeHead(404, { 'Content-Type': 'text/html' });
-                response.end('<h1>404 Not Found</h1>');
-            }
-            response.writeHead(200, { 'Content-Type': mime.lookup(pathname) });
-            response.end(data);
-        });
-    }
+router.get('/', (req, res) => {
+    res.send('Comments');
 });
 
-// Listen on port 8000, IP defaults to
+router.post('/add', urlencodedParser, (req, res) => {
+    const { comment, user_id, post_id } = req.body;
+    const sql = 'INSERT INTO comments (comment, user_id, post_id) VALUES (?, ?, ?)';
+    db.query(sql, [comment, user_id, post_id], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+router.get('/get', (req, res) => {
+    const { post_id } = req.query;
+    const sql = 'SELECT * FROM comments WHERE post_id = ?';
+    db.query(sql, [post_id], (err, result) => {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+module.exports = router;
