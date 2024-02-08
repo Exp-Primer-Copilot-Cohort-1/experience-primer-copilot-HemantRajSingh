193cda5016fe3ebc19531a9b627f5c08b0382fe0
Hemant Raj Singh <rajhemant84@gmail.com>
2024-02-08 05:36:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,28 +1,54 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create web server
 
-// Middleware
-app.use(express.json()); // Parse JSON body
+// Load the http module to create an http server.
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var mime = require('mime');
+var qs = require('querystring');
+var comments = [];
 
-// Data
-const comments = [
-  { id: 1, author: 'John', text: 'First comment!' },
-  { id: 2, author: 'Jane', text: 'Nice comment!' },
-  { id: 3, author: 'Tom', text: 'I like it!' },
-];
-
-// Routes
-app.get('/comments', (req, res) => {
-  res.send(comments);
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+    var pathname = url.parse(request.url).pathname;
+    if (pathname == '/') {
+        pathname = '/index.html';
+    }
+    if (pathname == '/index.html') {
+        fs.readFile(path.join(__dirname, pathname), function (err, data) {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/html' });
+                response.end('<h1>404 Not Found</h1>');
+            }
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(data);
+        });
+    } else if (pathname == '/comments') {
+        if (request.method == 'GET') {
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify(comments));
+        } else if (request.method == 'POST') {
+            var str = '';
+            request.on('data', function (data) {
+                str += data;
+            });
+            request.on('end', function () {
+                var comment = qs.parse(str);
+                comments.push(comment);
+                response.end();
+            });
+        }
+    } else {
+        fs.readFile(path.join(__dirname, pathname), function (err, data) {
+            if (err) {
+                response.writeHead(404, { 'Content-Type': 'text/html' });
+                response.end('<h1>404 Not Found</h1>');
+            }
+            response.writeHead(200, { 'Content-Type': mime.lookup(pathname) });
+            response.end(data);
+        });
+    }
 });
 
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  res.send(comment);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// Listen on port 8000, IP defaults to
